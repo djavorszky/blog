510b85a4003d8086063d973f570fbdccb25d30b3
djavorszky <daniel.javorszky@gmail.com>
2022-02-12 11:32:12 +0100

    Add image_caption shortcode

--- .eleventy.ext.js
+++ .eleventy.ext.js
@@ -17,28 +17,42 @@ const shortcodes = [
     },
     {
         name: "image",
-        func: function (src, alt, sizes = "(min-width: 1024px) 100vw, 50vw", cls = "img-fluid rounded mx-auto d-block") {
-            let options = {
-                widths: [300, 600],
-                formats: ["webp", "png"]
-            };
-
-            // generate images, while this is async we don’t wait
-            Image(src, options);
-
-            let imageAttributes = {
-                class: cls,
-                alt,
-                sizes,
-                loading: "lazy",
-                decoding: "async",
-            };
-            // get metadata even the images are not fully generated
-            let metadata = Image.statsSync(src, options);
-            return Image.generateHTML(metadata, imageAttributes);
+        func: generateImageSrc
+    },
+    {
+        name: "image_caption",
+        func: function (src, caption, alt, cls = "text-center", imgCls = "img-fluid rounded mx-auto d-block", sizes = "(min-width: 1024px) 100vw, 50vw") {
+            return outdent`
+            <div class="${cls}">
+                <figure class="figure text-center">
+                    ${generateImageSrc(src, alt, imgCls + " figure-img", sizes)}
+                    <figcaption class="figure-caption">${caption}</figcaption>
+                </figure>
+            </div>`
         }
     }
 ];
+
+function generateImageSrc(src, alt, cls = "img-fluid rounded mx-auto d-block", sizes = "(min-width: 1024px) 100vw, 50vw") {
+    let options = {
+        widths: [300, 600],
+        formats: ["webp", "png"]
+    };
+
+    // generate images, while this is async we don’t wait
+    Image(src, options);
+
+    let imageAttributes = {
+        class: cls,
+        alt,
+        sizes,
+        loading: "lazy",
+        decoding: "async",
+    };
+    // get metadata even the images are not fully generated
+    let metadata = Image.statsSync(src, options);
+    return Image.generateHTML(metadata, imageAttributes);
+};
 
 
 module.exports = {
