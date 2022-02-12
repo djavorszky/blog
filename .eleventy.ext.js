1606f1eb5a74a656b401cb64fcd03e59e4302ffd
djavorszky <daniel.javorszky@gmail.com>
2022-02-12 12:42:33 +0100

    Refactor

--- .eleventy.ext.js
+++ .eleventy.ext.js
@@ -1,19 +1,14 @@
-const outdent = require('outdent');
-const Image = require("@11ty/eleventy-img");
+const { generateImageSrc, imageCaption } = require("./ext/image");
+const outdent = require("outdent");
 
 const shortcodes = [
     {
         name: "alert",
-        func: function (level, message) {
-            return outdent`
-                <div class="alert alert-${level}">
-                    ${message}
-                </div>`
-        },
+        func: alert,
     },
     {
         name: "cit",
-        func: function () { return `<sup>[citation needed]</sup>` }
+        func: citationNeeded
     },
     {
         name: "image",
@@ -21,39 +16,21 @@ const shortcodes = [
     },
     {
         name: "image_caption",
-        func: function (src, caption, alt, cls = "text-center", imgCls = "img-fluid rounded mx-auto d-block", sizes = "(min-width: 1024px) 100vw, 50vw") {
-            return outdent`
-            <div class="${cls}">
-                <figure class="figure text-center">
-                    ${generateImageSrc(src, alt, imgCls + " figure-img", sizes)}
-                    <figcaption class="figure-caption">${caption}</figcaption>
-                </figure>
-            </div>`
-        }
+        func: imageCaption
     }
 ];
 
-function generateImageSrc(src, alt, cls = "img-fluid rounded mx-auto d-block", sizes = "(min-width: 1024px) 100vw, 50vw") {
-    let options = {
-        widths: [300, 600],
-        formats: ["webp", "png"]
-    };
 
-    // generate images, while this is async we don’t wait
-    Image(src, options);
+function alert(level, message) {
+    return outdent`
+        <div class="alert alert-${level}">
+            ${message}
+        </div>`
+}
 
-    let imageAttributes = {
-        class: cls,
-        alt,
-        sizes,
-        loading: "lazy",
-        decoding: "async",
-    };
-    // get metadata even the images are not fully generated
-    let metadata = Image.statsSync(src, options);
-    return Image.generateHTML(metadata, imageAttributes);
-};
-
+function citationNeeded() {
+    return `<sup>[citation needed]</sup>`
+}
 
 module.exports = {
     shortcodes
---
+++ ext/image.js
@@ -0,0 +1,36 @@
+const outdent = require("outdent");
+const Image = require("@11ty/eleventy-img");
+
+function imageCaption(src, caption, alt, cls = "text-center", imgCls = "img-fluid rounded mx-auto d-block", sizes = "(min-width: 1024px) 100vw, 50vw") {
+    return outdent`
+    <div class="${cls}">
+        <figure class="figure text-center">
+            ${generateImageSrc(src, alt, imgCls + " figure-img", sizes)}
+            <figcaption class="figure-caption">${caption}</figcaption>
+        </figure>
+    </div>`
+}
+
+function generateImageSrc(src, alt, cls = "img-fluid rounded mx-auto d-block", sizes = "(min-width: 1024px) 100vw, 50vw") {
+    let options = {
+        widths: [300, 600],
+        formats: ["webp", "png"]
+    };
+
+    // generate images, while this is async we don’t wait
+    Image(src, options);
+
+    let imageAttributes = {
+        class: cls,
+        alt,
+        sizes,
+        loading: "lazy",
+        decoding: "async",
+    };
+    // get metadata even the images are not fully generated
+    let metadata = Image.statsSync(src, options);
+    return Image.generateHTML(metadata, imageAttributes);
+};
+
+
+module.exports = { generateImageSrc, imageCaption }
